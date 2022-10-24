6ed760fe649911a427ad38d293f642cc0e40359d
uginim <hyeonukkim1990@gmail.com>
2022-10-24 19:03:10 +0900

    feat : 게시글 수정 추가

--- freelec-springboot2-webservice/src/main/java/com/jojoldo/book/springboot/service/posts/PostsService.java
+++ freelec-springboot2-webservice/src/main/java/com/jojoldo/book/springboot/service/posts/PostsService.java
@@ -1,14 +1,17 @@
 package com.jojoldo.book.springboot.service.posts;
 
 
+import com.jojoldo.book.springboot.domain.posts.Posts;
 import com.jojoldo.book.springboot.domain.posts.PostsRepository;
+import com.jojoldo.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldo.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldo.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class PostsService {
     private final PostsRepository postsRepository;
 
@@ -16,4 +19,22 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
+
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        posts.update(requestDto.getTitle(), requestDto.getContent()); // 그냥 값만 바꿈... 따로 db에 update하는게없음
+        // transaction이 끝나는 시점에 테이블의 변경분을 반영함
+
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
+    }
 }
+
