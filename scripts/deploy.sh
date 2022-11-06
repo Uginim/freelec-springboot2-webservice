c896269f6137443677bb4766b09c35ca17819ea7
uginim <kimugiugi@gmail.com>
2022-11-07 05:03:15 +0900

    fix : deploy script 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,11 +5,11 @@ PROJECT_NAME=freelec-springboot2-webservice
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/build/libs/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
